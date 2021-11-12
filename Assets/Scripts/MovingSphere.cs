6ddaf8054aa889094f86defe8ad61cd83fe5618d
lovebird <lovebirdsx@gmail.com>
2021-11-12 18:30:25 +0800

    up

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -86,6 +86,8 @@ public class MovingSphere : MonoBehaviour {
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
 
+        // 若包含了镜头,则以镜头为坐标系
+        // 否则使用默认坐标系
         if (playerInputSpace) {
             rightAxis = ProjectDirectionOnPlane(playerInputSpace.right, upAxis);
             forwardAxis = ProjectDirectionOnPlane(playerInputSpace.forward, upAxis);
@@ -94,9 +96,10 @@ public class MovingSphere : MonoBehaviour {
             forwardAxis = ProjectDirectionOnPlane(Vector3.forward, upAxis);
         }
 
+        // desiredJump在FixedUpdate中会用到,为了避免挑起按键被忽略掉,故而让状态保持
         desiredJump |= Input.GetButtonDown("Jump");
-        // desiresClimbing = Input.GetButton("Climb");
-        desiresClimbing = true;
+        desiresClimbing = Input.GetButton("Climb");
+        // desiresClimbing = true;
 
         meshRenderer.material = Climbing ? climbingMaterial : normalMaterial;
     }
@@ -114,6 +117,7 @@ public class MovingSphere : MonoBehaviour {
         if (Climbing) {
             velocity -= contactNormal * (maxClimbAcceleration * 0.9f * Time.deltaTime);
         } else if (OnGround && velocity.sqrMagnitude < 0.01f) {
+            // 处理抓地
             velocity += contactNormal * (Vector3.Dot(gravity, contactNormal) * Time.deltaTime);
         } else if (desiresClimbing && OnGround) {
             velocity += (gravity - contactNormal * (maxClimbAcceleration * 0.9f)) * Time.deltaTime;
@@ -133,18 +137,25 @@ public class MovingSphere : MonoBehaviour {
     }
 
     void UpdateState() {
+        // 记录上一次着地或者跳起后,物理更新的次数,从而方便进行抓地相关的操作
         stepsSinceLastGrounded += 1;
         stepsSinceLastJump += 1;
+
         velocity = body.velocity;
+
+        // 检查是否抓地,攀爬的时候也算
         if (CheckClimbing() || OnGround || SnapToGround() || CheckSteepContacts()) {
             stepsSinceLastGrounded = 0;
             if (stepsSinceLastJump > 1) {
                 jumpPhase = 0;
             }
+
+            // 相连的法线超过1个,则取其平均值
             if (groundContactCount > 1) {
                 contactNormal.Normalize();
             }
         } else {
+            // 没有黏住任何表面时,连接表面的法线向量为朝上
             contactNormal = upAxis;
         }
 
@@ -180,14 +191,20 @@ public class MovingSphere : MonoBehaviour {
         return false;
     }
 
+    // 确保抓地
     bool SnapToGround() {
+        // 不满足贴地检测条件则直接返回
         if (stepsSinceLastGrounded > 1 || stepsSinceLastJump <= 2) {
             return false;
         }
+
+        // 如果当前速度超过需要贴地检测速度的上限,则忽略
         float speed = velocity.magnitude;
         if (speed > maxSnapSpeed) {
             return false;
         }
+
+        // 以贴地的探测距离来检查
         if (!Physics.Raycast(
             body.position, -upAxis, out RaycastHit hit,
             probeDistance, probeMask
@@ -195,13 +212,17 @@ public class MovingSphere : MonoBehaviour {
             return false;
         }
 
+        // 确保当前的碰撞对象符合贴合的条件
         float upDot = Vector3.Dot(upAxis, hit.normal);
         if (upDot < GetMinDot(hit.collider.gameObject.layer)) {
             return false;
         }
 
+        // 使用该法线作为抓地法线
         groundContactCount = 1;
         contactNormal = hit.normal;
+
+        // 将速度中朝向该法线方向的速度分量去除
         float dot = Vector3.Dot(velocity, hit.normal);
         if (dot > 0f) {
             velocity = (velocity - hit.normal * dot).normalized * speed;
@@ -210,6 +231,7 @@ public class MovingSphere : MonoBehaviour {
         return true;
     }
 
+    // 检查是否有可能处在几个陡峭表面的中间,也当成是抓地
     bool CheckSteepContacts() {
         if (steepContactCount > 1) {
             steepNormal.Normalize();
@@ -230,11 +252,17 @@ public class MovingSphere : MonoBehaviour {
         if (Climbing) {
             acceleration = maxClimbAcceleration;
             speed = maxClimbSpeed;
+
+            // 攀爬时,移动的x轴为法线和向上坐标的叉乘
             xAxis = Vector3.Cross(contactNormal, upAxis);
+
+            // 同时,移动的z轴为向上坐标
             zAxis = upAxis;
         } else {
             acceleration = OnGround ? maxAcceleration : maxAirAcceleration;
             speed = OnGround && desiresClimbing ? maxClimbSpeed : maxSpeed;
+
+            // 没有攀爬时,移动的x轴为朝右,z轴为向前
             xAxis = rightAxis;
             zAxis = forwardAxis;
         }
@@ -247,10 +275,8 @@ public class MovingSphere : MonoBehaviour {
 
         float maxSpeedChange = acceleration * Time.deltaTime;
 
-        float newX =
-            Mathf.MoveTowards(currentX, playerInput.x * speed, maxSpeedChange);
-        float newZ =
-            Mathf.MoveTowards(currentZ, playerInput.y * speed, maxSpeedChange);
+        float newX = Mathf.MoveTowards(currentX, playerInput.x * speed, maxSpeedChange);
+        float newZ = Mathf.MoveTowards(currentZ, playerInput.y * speed, maxSpeedChange);
 
         velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
     }
@@ -273,12 +299,20 @@ public class MovingSphere : MonoBehaviour {
 
         stepsSinceLastJump = 0;
         jumpPhase += 1;
+
+        // 由公式 V = Sqrt(-2 * g * h), 算出起跳速度
         float jumpSpeed = Mathf.Sqrt(2f * gravity.magnitude * jumpHeight);
+        
+        // 最终跳跃方向需要和向上的向量相加,当攀爬时,表现为朝斜上跳跃
         jumpDirection = (jumpDirection + upAxis).normalized;
+
+        // 如果在跳跃方向上本身已经存在速度,那么就要确保最终的跳跃速度不超过预期的跳跃速度
         float alignedSpeed = Vector3.Dot(velocity, jumpDirection);
         if (alignedSpeed > 0f) {
             jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
         }
+
+        // 注意此处是加上速度,而不是直接修改速度
         velocity += jumpDirection * jumpSpeed;
     }
 
@@ -290,17 +324,22 @@ public class MovingSphere : MonoBehaviour {
         EvaluateCollision(collision);
     }
 
+    // EvaluateCollision调用时机在Physic.Simulate()之后,也就是所有GameObject的FixedUpdate()之后
     void EvaluateCollision(Collision collision) {
+        // 楼梯也作为可以连接的表面
         int layer = collision.gameObject.layer;
         float minDot = GetMinDot(layer);
+
         for (int i = 0; i < collision.contactCount; i++) {
             Vector3 normal = collision.GetContact(i).normal;
             float upDot = Vector3.Dot(upAxis, normal);
             if (upDot >= minDot) {
+                // 将所有地面的法线向量相加,到时再求平均
                 groundContactCount += 1;
                 contactNormal += normal;
                 connectedBody = collision.rigidbody;
             } else {
+                // 不能成为地面,且法线垂直分量大于0(此处判断为>-0.01f),则将其判断为陡峭表面
                 if (upDot > -0.01f) {
                     steepContactCount += 1;
                     steepNormal += normal;
@@ -308,10 +347,7 @@ public class MovingSphere : MonoBehaviour {
                         connectedBody = collision.rigidbody;
                     }
                 }
-                if (
-                    desiresClimbing && upDot >= minClimbDotProduct &&
-                    (climbMask & (1 << layer)) != 0
-                ) {
+                if (desiresClimbing && upDot >= minClimbDotProduct && (climbMask & (1 << layer)) != 0) {
                     climbContactCount += 1;
                     climbNormal += normal;
                     lastClimbNormal = normal;
